57835dd65714c7fa42701df287b011c6bda0c7fc
Akash Banger <akashbanger2@gmail.com>
2024-12-01 09:17:33 +0530

    added metadata for input video

--- metadata.py
+++ metadata.py
@@ -1,216 +1,65 @@
 players_metadata = {
     "teams": {
-        "Pittsburgh Penguins Roster": {
+        "team1": {
             "players": {
-                # Centers
+                "88": {
+                    "name": "John Anderson",
+                    "position": "Center"
+                },
                 "55": {
-                    "name": "Noel Acciari",
-                    "position": "Center"
-                },
-                "87": {
-                    "name": "Sidney Crosby",
-                    "position": "Center"
-                },
-                "19": {
-                    "name": "Cody Glass",
-                    "position": "Center"
-                },
-                "46": {
-                    "name": "Blake Lizotte",
-                    "position": "Center"
-                },
-                "71": {
-                    "name": "Evgeni Malkin",
-                    "position": "Center"
-                },
-                "53": {
-                    "name": "Philip Tomasino",
-                    "position": "Center"
-                },
-                # Left Wings
-                "72": {
-                    "name": "Anthony Beauvillier",
+                    "name": "Mike Thompson",
                     "position": "Left Wing"
                 },
                 "8": {
-                    "name": "Michael Bunting",
-                    "position": "Left Wing"
-                },
-                "74": {
-                    "name": "Tanner Howe",
-                    "position": "Left Wing"
-                },
-                "83": {
-                    "name": "Matt Nieto",
-                    "position": "Left Wing"
-                },
-                "10": {
-                    "name": "Drew O'Connor",
-                    "position": "Left Wing"
-                },
-                # Right Wings
-                "13": {
-                    "name": "Kevin Hayes",
+                    "name": "Chris Roberts",
                     "position": "Right Wing"
                 },
-                "18": {
-                    "name": "Jesse Puljujarvi",
-                    "position": "Right Wing"
-                },
-                "48": {
-                    "name": "Valtteri Puustinen",
-                    "position": "Right Wing"
-                },
-                "67": {
-                    "name": "Rickard Rakell",
-                    "position": "Right Wing"
-                },
-                "17": {
-                    "name": "Bryan Rust",
-                    "position": "Right Wing"
-                },
-                # Defense
-                "27": {
-                    "name": "Ryan Graves",
+                "25": {
+                    "name": "David Wilson",
                     "position": "Defense"
                 },
-                "24": {
-                    "name": "Matt Grzelcyk",
-                    "position": "Defense"
-                },
-                "65": {
-                    "name": "Erik Karlsson",
-                    "position": "Defense"
-                },
-                "58": {
-                    "name": "Kris Letang",
-                    "position": "Defense"
-                },
-                "28": {
-                    "name": "Marcus Pettersson",
-                    "position": "Defense"
-                },
-                "38": {
-                    "name": "Owen Pickering",
-                    "position": "Defense"
-                },
-                "5": {
-                    "name": "Ryan Shea",
-                    "position": "Defense"
+                "20": {
+                    "name": "Peter Mitchell",
+                    "position": "Center"
                 },
                 "3": {
-                    "name": "Jack St. Ivany",
+                    "name": "James Cooper",
                     "position": "Defense"
                 },
-                # Goalies
-                "35": {
-                    "name": "Tristan Jarry",
-                    "position": "Goalie"
-                },
-                "39": {
-                    "name": "Alex Nedeljkovic",
-                    "position": "Goalie"
+                "50": {
+                    "name": "Ryan Phillips",
+                    "position": "Right Wing"
                 }
             }
         },
-        "Boston Bruins Roster": {
-
+        "team2": {
             "players": {
-                # Centers
-                "19": {
-                    "name": "Johnny Beecher",
-                    "position": "Center"
-                },
-                "13": {
-                    "name": "Charlie Coyle",
-                    "position": "Center"
-                },
-                "11": {
-                    "name": "Trent Frederic",
-                    "position": "Center"
-                },
-                "39": {
-                    "name": "Morgan Geekie",
+                "28": {
+                    "name": "Steve Baker",
                     "position": "Center"
                 },
                 "90": {
-                    "name": "Tyler Johnson",
-                    "position": "Center"
-                },
-                "47": {
-                    "name": "Mark Kastelic",
-                    "position": "Center"
-                },
-                "28": {
-                    "name": "Elias Lindholm",
-                    "position": "Center"
-                },
-                "26": {
-                    "name": "Marc McLaughlin",
-                    "position": "Center"
-                },
-                "18": {
-                    "name": "Pavel Zacha",
-                    "position": "Center"
-                },
-                # Left Wings
-                "45": {
-                    "name": "Cole Koepke",
+                    "name": "Tom Harrison",
                     "position": "Left Wing"
                 },
-                "63": {
-                    "name": "Brad Marchand",
-                    "position": "Left Wing"
+                "65": {
+                    "name": "Mark Stevens",
+                    "position": "Defense"
                 },
-                # Right Wings
-                "55": {
-                    "name": "Justin Brazeau",
+                "77": {
+                    "name": "Eric Johnson",
                     "position": "Right Wing"
                 },
-                "88": {
-                    "name": "David Pastrnak",
-                    "position": "Right Wing"
+                "14": {
+                    "name": "Daniel Brown",
+                    "position": "Center"
                 },
-                # Defense
-                "25": {
-                    "name": "Brandon Carlo",
+                "37": {
+                    "name": "Paul Davis",
                     "position": "Defense"
                 },
-                "27": {
-                    "name": "Hampus Lindholm",
-                    "position": "Defense"
-                },
-                "6": {
-                    "name": "Mason Lohrei",
-                    "position": "Defense"
-                },
-                "73": {
-                    "name": "Charlie McAvoy",
-                    "position": "Defense"
-                },
-                "52": {
-                    "name": "Andrew Peeke",
-                    "position": "Defense"
-                },
-                "75": {
-                    "name": "Alec Regula",
-                    "position": "Defense"
-                },
-                "29": {
-                    "name": "Parker Wotherspoon",
-                    "position": "Defense"
-                },
-                "91": {
-                    "name": "Nikita Zadorov",
-                    "position": "Defense"
-                },
-                # Goalies
-                "70": {
-                    "name": "Joonas Korpisalo",
-                    "position": "Goalie"
-                },
-                "1": {
-                    "name": "Jeremy Swayman",
+                "35": {
+                    "name": "Kevin Martin",
                     "position": "Goalie"
                 }
             }
